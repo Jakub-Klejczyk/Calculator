d72604d7049477c1bb7075ffb032acfd37916ee2
Jakub-Klejczyk <80176594+Jakub-Klejczyk@users.noreply.github.com>
2022-03-05 21:29:21 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -2,21 +2,52 @@ const firstNumber = document.getElementById("firstNumber");
 const secondNumber = document.getElementById("secondNumber");
 const resultForm = document.getElementById("result");
 //
-const btnAddition = document.getElementById("addition");
-const btnSubtraction = document.getElementById("subtraction");
-const btnMultiplication = document.getElementById("multiplication");
-const btnDivision = document.getElementById("division");
 
 let result = "";
-let firstResult = parseInt(firstNumber.value);
-let secondResult = parseInt(secondNumber.value);
+let firstResult = "";
+let secondResult = "";
+
+function parsing() {
+  firstResult = parseInt(firstNumber.value);
+  secondResult = parseInt(secondNumber.value);
+}
 
 function onAddition() {
-  if (typeof firstResult == "number" && typeof secondResult == "number") {
-    result = firstResult + secondResult;
-    console.log(result);
-    resultForm.value = result;
-  } else {
+  parsing();
+  if (isNaN(firstResult) || isNaN(secondResult)) {
     alert("Type of input is not an number");
+    return;
   }
+  result = firstResult + secondResult;
+  resultForm.value = result;
+}
+
+function onSubtraction() {
+  parsing();
+  if (isNaN(firstResult) || isNaN(secondResult)) {
+    alert("Type of input is not an number");
+    return;
+  }
+  result = firstResult - secondResult;
+  resultForm.value = result;
+}
+
+function onMultiplication() {
+  parsing();
+  if (isNaN(firstResult) || isNaN(secondResult)) {
+    alert("Type of input is not an number");
+    return;
+  }
+  result = firstResult * secondResult;
+  resultForm.value = result;
+}
+
+function onDivision() {
+  parsing();
+  if (isNaN(firstResult) || isNaN(secondResult)) {
+    alert("Type of input is not an number");
+    return;
+  }
+  result = firstResult / secondResult;
+  resultForm.value = result;
 }
